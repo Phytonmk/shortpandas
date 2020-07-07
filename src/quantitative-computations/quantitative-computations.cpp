3872ace182b1ee13a536448ae476fd22255c3542
alexia <sanyok003@gmail.com>
2020-07-07 15:44:49 +0300

    adding some computations

--- src/quantitative-computations/quantitative-computations.cpp
+++ src/quantitative-computations/quantitative-computations.cpp
@@ -5,8 +5,9 @@
 #include <set>
 #include <iostream>
 #include "../csv-reader/csv-reader.h"
+#include <string>
 
-int quantitativeComputations (std::vector<InputRow> data)
+int quantitativeComputations (std::vector<InputRow> data, int &all, int &unique_values, int &empty_values)
 {
     std::multiset <long double> unique;
     for(int i=0;i<data.size();i++) //записываем все значения в коллекцию
@@ -21,7 +22,7 @@ int quantitativeComputations (std::vector<InputRow> data)
         unique.insert(data.at(i).percapita);
         unique.insert(data.at(i).population);
     }
-    int unique_values=0;
+    unique_values=0;
     for (int i=0; i<data.size();i++)//подсчет числа вхождений значения во множество
     {
         if(unique.count(data.at(i).crop_land)==1)
@@ -43,9 +44,24 @@ int quantitativeComputations (std::vector<InputRow> data)
         if(unique.count(data.at(i).population)==1)
             unique_values++;
     }
-    int empty_values=0;
+    empty_values=0;
     empty_values=unique.count(-1);//количество пустых значений
     int all_values=0;
-    all_values=unique.size()-empty_values;//количество всех значений без пустых
+    all_values=unique.size()-empty_values;//количество значений без пустых и без годов и стран
+    std::multiset <std::string> countries;
+    std::multiset <int> years;
+    for (int i=0;i<data.size();i++)
+    {
+        countries.insert(data.at(i).country);
+    }
+    int all_countries = 0;
+    all_countries = countries.size();//количество данных в столбце страны
+    for (int i=0;i<data.size();i++)
+    {
+        years.insert(data.at(i).year);
+    }
+    int all_years = 0;
+    all_years = years.size();//количество данных в столбце год
+    all=all_values+all_years+all_countries;//количество всех данных без пустых значений
     return 0;
 }
