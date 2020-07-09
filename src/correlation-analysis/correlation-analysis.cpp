553e12398115b883b44100e82704fb99d2b85a9d
alexia <sanyok003@gmail.com>
2020-07-09 17:10:36 +0300

    additional check

--- src/correlation-analysis/correlation-analysis.cpp
+++ src/correlation-analysis/correlation-analysis.cpp
@@ -57,29 +57,38 @@ std::vector<correlRate> correlationAnalysis (std::vector <InputRow> data, std::v
                 y_diff.push_back(y[j]-y_average);
             }
         }
-        for (int j=0;j<x_diff.size();j++)//вычисление квадратов разностей
+        if((x_diff.size()!=0) && y_diff.size()!=0)//проверка на возможность посчитать коэффициент Пирсона
         {
-            x_diffSq.push_back((x_diff[j]*x_diff[j]));
-            y_diffSq.push_back((y_diff[j]*y_diff[j]));
+            for (int j=0;j<x_diff.size();j++)//вычисление квадратов разностей
+            {
+                x_diffSq.push_back((x_diff[j]*x_diff[j]));
+                y_diffSq.push_back((y_diff[j]*y_diff[j]));
+            }
+            long double currMulti=0, currSum=0;
+            for(int j=0;j<x_diff.size();j++)//подсчет числителя (сумма произведений разностей)
+            {
+                currMulti=x_diff[j]*y_diff[j];
+                currSum=currSum+currMulti;
+            }
+            long double sumSq_x=0, sumSq_y=0;
+            for (int j=0;j<x_diffSq.size();j++)//подсчет суммы квадратов разностей выборки Х
+            {
+                sumSq_x=sumSq_x+x_diffSq[j];
+            }
+            for (int j=0;j<y_diffSq.size();j++)//подсчет суммы квадратов разностей выборки Y
+            {
+                sumSq_y=sumSq_y+y_diffSq[j];
+            }
+            current_rate.country=current_country;
+            current_rate.rate=currSum/(sqrt(sumSq_x*sumSq_y));
+            correlation_analysis.push_back(current_rate);
         }
-        long double currMulti=0, currSum=0;
-        for(int j=0;j<x_diff.size();j++)//подсчет числителя (сумма произведений разностей)
+        else
         {
-            currMulti=x_diff[j]*y_diff[j];
-            currSum=currSum+currMulti;
+            current_rate.country=current_country;
+            current_rate.rate=-11;//-11 означает, что коэффициент не удалось посчитать
+            correlation_analysis.push_back(current_rate);
         }
-        long double sumSq_x=0, sumSq_y=0;
-        for (int j=0;j<x_diffSq.size();j++)//подсчет суммы квадратов разностей выборки Х
-        {
-            sumSq_x=sumSq_x+x_diffSq[j];
-        }
-        for (int j=0;j<y_diffSq.size();j++)//подсчет суммы квадратов разностей выборки Y
-        {
-            sumSq_y=sumSq_y+y_diffSq[j];
-        }
-        current_rate.country=current_country;
-        current_rate.rate=currSum/(sqrt(sumSq_x*sumSq_y));
-        correlation_analysis.push_back(current_rate);
         x.clear();
         y.clear();
         x_diff.clear();
